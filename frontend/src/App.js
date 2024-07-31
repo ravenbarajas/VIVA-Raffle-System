e04b92606b413764c121a0cdc4435e09226389a8
Carl Raven Barajas <56469543+ravenbarajas@users.noreply.github.com>
2024-07-31 17:53:47 +0800

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RafflePage from './components/pages/RafflePage';
+
+function App() {
+  return (
+    <div className="App">
+      <RafflePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/pages/RafflePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/RafflePage.css';
+
+function RafflePage() {
+  return (
+    <div className="rafflePage-container">
+        <div className='rafflePage-body'>
+            <button>
+                Click me for game
+            </button>
+        </div>
+    </div>
+  );
+}
+
+export default RafflePage;
